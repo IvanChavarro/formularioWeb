977fdca2f00a0d6d64dbebb631471d0271069da1
Ivan <ivanchavarro99@gmail.com>
2020-10-26 02:43:09 -0500

    Vista completa, falta complementar la parte del controlador

---
+++ src/main/java/com/mycompany/form/web/IndexController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.form.web;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Iván
+ */
+@Named
+public class IndexController{
+
+    /**
+     * Creates a new instance of IndexController
+     */
+    private String nombre, seleccion;
+
+    public String getSeleccion() {
+        return seleccion;
+    }
+
+    public void setSeleccion(String seleccion) {
+        this.seleccion = seleccion;
+    }
+
+    private String[] nivelEducativo = {"Bachiller", "Tecnico", "Tecnologo", "Profesional"};
+
+    public String[] getNivelEducativo() {
+        return nivelEducativo;
+    }
+
+    public void setNivelEducativo(String[] nivelEducativo) {
+        this.nivelEducativo = nivelEducativo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void mostrarDatos() {
+        System.out.println(this.nombre);
+    }
+
+}
