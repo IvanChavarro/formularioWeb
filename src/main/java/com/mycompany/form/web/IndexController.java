b66cbc02b6e6de781281a90187b38729e000d65a
ktibaquira <55606123+ktibaquira@users.noreply.github.com>
2020-10-27 00:40:59 -0500

    Comentarios Completos

--- src/main/java/com/mycompany/form/web/IndexController.java
+++ src/main/java/com/mycompany/form/web/IndexController.java
@@ -20,34 +20,83 @@ import javax.inject.Named;
 public class IndexController {
 
     /**
-     * Creates a new instance of IndexController
-     */
-    /**
-     * Variables de carga desde el view
+     * Variable privada que guarda el nombre
      */
     private String nombre;
+    /**
+     * Variable privada que guarda la seleccion
+     */
     private String seleccion;
+    /**
+     * Variable privada que guarda la cedula
+     */
     private String cedula;
+    /**
+     * Variable privada que guarda el apellido
+     */
     private String apellido;
+    /**
+     * Variable privada que guarda nivel educativo
+     */
     private String nivEducativo;
+    /**
+     * Variable privada que guarda los idiomas
+     */
     private String idiomas;
+    /**
+     * Variable privada que guarda los dias trabajados
+     */
     private String diasTrabajados;
+    /**
+     * Variable privada que guarda el sueldo base
+     */
     private String sueldoBase;
+    /**
+     * Variable privada que guarda los niveles educativos
+     */
     private String[] nivelEducativo = {"Bachiller", "Tecnico", "Tecnologo", "Profesional", "Magister"};
+    /**
+     * Arreglo privado que guarda la seleccion de idiomas
+     */
     private List<String> seleccionIdiomas;
+    /**
+     * Arreglo privado que guarda los idiomas
+     */
     private List<String> guardarIdiomas;
+    /**
+     * Variable privada que guarda el mensaje del resultado
+     */
     private String mensajeResultado;
 
+    /**
+     * metodo get de mensajeResultado
+     *
+     * @return MensajeResultado
+     */
     public String getMensajeResultado() {
         return mensajeResultado;
     }
 
+    /**
+     * metodo set de mensajeResultado
+     *
+     * @param mensajeResultado
+     */
     public void setMensajeResultado(String mensajeResultado) {
         this.mensajeResultado = mensajeResultado;
     }
+    /**
+     * Variable privada que guarda el pago final
+     */
+    private int pagoFinal;
+    /**
+     * Variable privada que guarda el auxiliar
+     */
+    private int aux;
 
-    private int pagoFinal, aux;
-
+    /**
+     * constructor
+     */
     public IndexController() {
         seleccionIdiomas = new ArrayList<String>();
         seleccionIdiomas.add("Mandarín");
@@ -55,18 +104,23 @@ public class IndexController {
         seleccionIdiomas.add("Inglés");
         seleccionIdiomas.add("Portugues");
         seleccionIdiomas.add("Vientoñol");
-
     }
 
+    /**
+     * metodo click que muestra el mensaje
+     */
     public void click() {
         pagoFinal = Integer.parseInt(diasTrabajados) * Integer.parseInt(sueldoBase);
         estudios();
         idiomas();
-        mensajeResultado = "Señor "+this.nombre+" "
-                +this.apellido+", identificado con C.C "+this.cedula+
-                ", su salario será de "+pagoFinal;
+        mensajeResultado = "Señor " + this.nombre + " "
+                + this.apellido + ", identificado con C.C " + this.cedula
+                + ", su salario será de " + pagoFinal;
     }
 
+    /**
+     * metodo estudios hace los calculos dependiendo del nivel educativo
+     */
     private void estudios() {
         switch (this.nivEducativo) {
             case "Tecnico":
@@ -84,95 +138,208 @@ public class IndexController {
         }
     }
 
+    /**
+     * metodo de idiomas realiza el calculo del pago final
+     */
     private void idiomas() {
-        aux =  guardarIdiomas.size()*10000;
+        aux = guardarIdiomas.size() * 10000;
         pagoFinal += aux;
     }
 
+    /**
+     * metodo get de cedula
+     *
+     * @return cedula
+     */
     public String getCedula() {
         return cedula;
     }
 
+    /**
+     * metodo set de cedula
+     *
+     * @param cedula
+     */
     public void setCedula(String cedula) {
         this.cedula = cedula;
     }
 
+    /**
+     * metodo get de apellido
+     *
+     * @return apellido
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * metodo set de apellido
+     *
+     * @param apellido
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    /**
+     * metodo get de nivEducativo
+     *
+     * @return nivEducativo
+     */
     public String getNivEducativo() {
         return nivEducativo;
     }
 
+    /**
+     * metodo set de nivEducativo
+     *
+     * @param nivEducativo
+     */
     public void setNivEducativo(String nivEducativo) {
         this.nivEducativo = nivEducativo;
     }
 
+    /**
+     * metodo get de idiomas
+     *
+     * @return idiomas
+     */
     public String getIdiomas() {
         return idiomas;
     }
 
+    /**
+     * metodo set de idiomas
+     *
+     * @param idiomas
+     */
     public void setIdiomas(String idiomas) {
         this.idiomas = idiomas;
     }
 
+    /**
+     * metodo get de diasTrabajados
+     *
+     * @return diasTrabajados
+     */
     public String getDiasTrabajados() {
         return diasTrabajados;
     }
 
+    /**
+     * metodo set de diasTrabajados
+     *
+     * @param diasTrabajados
+     */
     public void setDiasTrabajados(String diasTrabajados) {
         this.diasTrabajados = diasTrabajados;
     }
 
+    /**
+     * metodo get de seleccion
+     *
+     * @return seleccion
+     */
     public String getSeleccion() {
         return seleccion;
     }
 
+    /**
+     * metodo set de seleccion
+     *
+     * @param seleccion
+     */
     public void setSeleccion(String seleccion) {
         this.seleccion = seleccion;
     }
 
+    /**
+     * metodo get de nivelEducativo
+     *
+     * @return nivelEducativo
+     */
     public String[] getNivelEducativo() {
         return nivelEducativo;
     }
 
+    /**
+     * metodo set de nivelEducativo
+     *
+     * @param nivelEducativo
+     */
     public void setNivelEducativo(String[] nivelEducativo) {
         this.nivelEducativo = nivelEducativo;
     }
 
+    /**
+     * metodo get de nombre
+     *
+     * @return nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * metodo set de nombre
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * metodo get de seleccionIdiomas
+     *
+     * @return seleccionIdiomas
+     */
     public List<String> getSeleccionIdiomas() {
         return seleccionIdiomas;
     }
 
+    /**
+     * metodo set de seleccionIdiomas
+     *
+     * @param seleccionIdiomas
+     */
     public void setSeleccionIdiomas(List<String> seleccionIdiomas) {
         this.seleccionIdiomas = seleccionIdiomas;
     }
 
+    /**
+     * metodo get de guardarIdiomas
+     *
+     * @return
+     */
     public List<String> getGuardarIdiomas() {
         return guardarIdiomas;
     }
 
+    /**
+     * metodo set de guardarIdiomas
+     *
+     * @param guardarIdiomas
+     */
     public void setGuardarIdiomas(List<String> guardarIdiomas) {
         this.guardarIdiomas = guardarIdiomas;
     }
 
+    /**
+     * metodo get de sueldoBase
+     *
+     * @return sueldoBase
+     */
     public String getSueldoBase() {
         return sueldoBase;
     }
 
+    /**
+     * metodo set de sueldoBase
+     *
+     * @param sueldoBase
+     */
     public void setSueldoBase(String sueldoBase) {
         this.sueldoBase = sueldoBase;
     }
